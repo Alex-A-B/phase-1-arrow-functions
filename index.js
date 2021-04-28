838d568beff54fcfe9c031e1e000fbf10f24d1db
Alex-A-B <alexjamesbelton@gmail.com>
2021-04-28 09:59:54 +0100

    assignment completed

---
+++ index.js
@@ -0,0 +1,13 @@
+// function expression called divide which divides 2000 by 100
+
+const divide = function() {
+    return 2000 / 100
+}
+
+// arrow function called square, takes one param and mutiplies by itself
+
+const square = (parameter1) => parameter1 * parameter1;
+
+// arrow function called add, takes two param and ads them together
+
+const add = (param1, param2) => param1 + param2;
